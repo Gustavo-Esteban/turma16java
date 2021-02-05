9576a3eff9ba4e410c37ceba5ca083c1d18d37a5
Esteban <gustavoesteban13@hotmail.com>
2021-02-05 14:16:39 -0300

    Lista 8

--- java/projetojava/src/lista8/Produtos.java
+++ java/projetojava/src/lista8/Produtos.java
@@ -1,5 +1,26 @@
 package lista8;
 
 public class Produtos {
+	private String produtos;
+	private double preco;
+	public Produtos(String produtos, double preco) {
+		super();
+		this.produtos = produtos;
+		this.preco = preco;
+	}
+	public String getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(String produtos) {
+		this.produtos = produtos;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
 
 }
--- java/projetojava/src/lista8/TesteList.java
+++ java/projetojava/src/lista8/TesteList.java
@@ -18,12 +18,53 @@ Atualizar dados da list.
 Apresentar todos os dados da list.
 */
 		Scanner ler = new Scanner (System.in);
+		List <Produtos> lista = new ArrayList <>();
 		
 		int numero;
 		System.out.println("Digite quantos produtos: ");
 		numero = ler.nextInt();
 		
-		List lista = new ArrayList <>();
+		for(int x = 0; x < numero; x++)
+		{
+			System.out.println("Digite nome do produto: ");
+			String nome = ler.next();
+			System.out.println("Digite o preço: ");
+			double preco = ler.nextDouble();
+			lista.add(new Produtos(nome, preco));
+			System.out.println("Deseja fazer alguma alteração? 1-Alterar 2-Remover 0-Sair\nInsira aqui:");
+			int op = ler.nextInt();
+			while(op != 0)
+			{
+				
+				if(op == 1)
+				{
+					lista.remove(x);
+					System.out.println("Digite nome do produto: ");
+					String nome1 = ler.next();
+					System.out.println("Digite o preço: ");
+					double preco2 = ler.nextDouble();
+					lista.add(new Produtos(nome1, preco2));
+				}
+	
+				else if(op == 2)
+					{
+						
+						lista.remove(x);
+						
+					}	
+				System.out.println("Deseja fazer alguma alteração? 1-Alterar 2-Remover 0-Sair\nInsira aqui:");
+				op = ler.nextInt();
+			}
+			
+		}
+		System.out.println();
+		System.out.println("PRODUTOS: ");
+		for(Produtos prod : lista)
+		{
+			System.out.println(prod.getProdutos()+" = R$ "+prod.getPreco());
+		}
+		
+		
 		
 
 	}
