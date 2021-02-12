56114f01a903e1122fc08ca99f033ab64c174313
Esteban <gustavoesteban13@hotmail.com>
2021-02-12 12:52:13 -0300

    Correção

--- MySQL/Lista1/Atividade 2.sql
+++ MySQL/Lista1/Atividade 2.sql
@@ -1,30 +1,29 @@
-create database ecommerce;
+create database e_commerce;
 
-use ecommerce;
+use e_commerce;
 
 create table produtos(
 	id bigint auto_increment,
     nome varchar(255)not null,
-    descricao varchar(255)not null,
+    cor varchar(255)not null,
     qnt int not null,
     preco float not null,
     primary key (id)
 );
 
-insert into produtos (nome,descricao,qnt,preco) values ('X-Burguer','Pão-Carne-Queijo',10,10.00);
-insert into produtos (nome,descricao,qnt,preco) values ('X-Salada','Pão-Carne-Queijo-Salada',10,11.00);
-insert into produtos (nome,descricao,qnt,preco) values ('X-Bacon','Pão-Carne-Queijo-Bacon',10,12.00);
-insert into produtos (nome,descricao,qnt,preco) values ('X-Bacon Egg','Pão-Carne-Queijo-Bacon-Ovo',10,13.00);
-insert into produtos (nome,descricao,qnt,preco) values ('X-Egg','Pão-Ovo-Queijo',10,8.00);
-insert into produtos (nome,descricao,qnt,preco) values ('X-Frango','Pão-Frango-Queijo',10,10.00);
-insert into produtos (nome,descricao,qnt,preco) values ('X-Frango Egg','Pão-Frango-Queijo-Ovo',10,11.00);
-insert into produtos (nome,descricao,qnt,preco) values ('X-Tudo','Pão-Carne-Queijo-Bacon-Ovo-Frango-Salada',10,15.00);
-delete from produtos where id = 1;
+insert into produtos (nome,cor,qnt,preco) values ('Mouse','Preto',10,300);
+insert into produtos (nome,cor,qnt,preco) values ('Teclado','Rosa',10,550);
+insert into produtos (nome,cor,qnt,preco) values ('Fone','Quartz',10,750);
+insert into produtos (nome,cor,qnt,preco) values ('Notebook','Dourado',10,3000);
+insert into produtos (nome,cor,qnt,preco) values ('Celular','Rose',10,1800);
+insert into produtos (nome,cor,qnt,preco) values ('PC','Vermelho',10,5500);
+insert into produtos (nome,cor,qnt,preco) values ('WebCam','Prata',10,50);
+insert into produtos (nome,cor,qnt,preco) values ('Controle','Branco',10,150);
 
-select * from produtos where preco > 12.00;
-select * from produtos where preco < 12.00;
+select * from produtos where preco > 500;
+select * from produtos where preco < 500;
 
-update produtos set preco = 9.00
-where id = 6;
+update produtos set preco = 75
+where id = 7;
 
-select * from produtos;	
+select * from produtos;
